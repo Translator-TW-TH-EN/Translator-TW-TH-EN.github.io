71aae85f9f8cfba3041ab6476858e662cdc7cdae
Translator-TW-TH-EN <monzero49sandbox@gmail.com>
2024-01-25 01:02:26 +0800

    Site updated: 2024-01-25 01:02:26

--- data/browserCheck.js
+++ data/browserCheck.js
@@ -1,25 +1,23 @@
-// 網頁載入完檢查為預設瀏覽器
-document.addEventListener('DOMContentLoaded', function () {
-	try {
-		var u = navigator.userAgent
-		let isLineApp = /Line/.test(u);
-		let isIOS = /iPad|iPhone|iPod/.test(u) && !window.MSStream;
-		let isAndroid = /Android/.test(u);
+// 檢查是什麼瀏覽器開啟
+try {
+	var u = navigator.userAgent
+	let isLineApp = /Line/.test(u);
+	let isIOS = /iPad|iPhone|iPod/.test(u) && !window.MSStream;
+	let isAndroid = /Android/.test(u);
 
 
-		if (isLineApp) {
-			window.location.href += "?openExternalBrowser=1"
-		}
-		else if (isIOS) {
-			window.location.href = 'googlechromes://www.tovia.com/';
-		}
-		else if (isAndroid) {
-			window.location.href = 'googlechromes://navigate?url=www.tovia.com/';
-		}
-		else {
-			return;
-		}
-	} catch (e) {
-		alert(e)
+	if (isLineApp) {
+		window.location.href += "?openExternalBrowser=1"
 	}
-});
+	else if (isIOS) {
+		window.location.href = 'googlechromes://translator-tw-th-en.github.io/order/';
+	}
+	else if (isAndroid) {
+		window.location.href = 'googlechromes://navigate?url=translator-tw-th-en.github.io/order/';
+	}
+	else {
+		return;
+	}
+} catch (e) {
+	alert(e)
+}
