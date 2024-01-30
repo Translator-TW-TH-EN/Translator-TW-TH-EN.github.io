dd34e9e7d1179303a6a462b17549963edb6b2554
Translator-TW-TH-EN <monzero49sandbox@gmail.com>
2024-01-31 01:23:02 +0800

    Site updated: 2024-01-31 01:23:02

--- data/loader.js
+++ data/loader.js
@@ -14,3 +14,14 @@ if (isLoaderEnabled) {
         .then(html => document.body.innerHTML += html)
         .catch(error => console.error('加載 loader.html 出錯：', error));
 }
+
+// 若一直不停加載超過設定秒數，則強制跳轉
+var timeoutId = setTimeout(function () {
+    if (document.querySelector('.loader').style.display === 'flex') {
+        window.location.href = "/login";
+    }
+}, 5000);
+// 在頁面即將卸載時清理計時器
+window.addEventListener('beforeunload', function () {
+    clearTimeout(timeoutId);
+});
